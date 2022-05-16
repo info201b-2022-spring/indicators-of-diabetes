c54205580940aa2089b613fcfb2f9145c036330a
daniellatsing <102693164+daniellatsing@users.noreply.github.com>
2022-05-16 10:37:38 -0700

    prompt in summary file

---
+++ summary.R
@@ -0,0 +1,7 @@
+# Summary ---------------------------------------------------------------
+# Store summary information into a list.
+# Compute at least 5 different values from your data that you believe are pertinent to share.
+
+
+# Write a paragraph of reflection on the summary information calculated by your
+# summary information function.
