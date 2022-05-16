47bc2321541f0f3134b58cf9d70dca3e481bb6e4
daniellatsing <102693164+daniellatsing@users.noreply.github.com>
2022-05-16 13:56:19 -0700

    reformatted data set and added questions to summary

--- summary.R
+++ summary.R
@@ -1,7 +1,21 @@
 # Summary ---------------------------------------------------------------
 # Store summary information into a list.
 # Compute at least 5 different values from your data that you believe are pertinent to share.
+cost_df <- read.csv("/Users/daniella/desktop/INFO 201/final-projects-daniellatsing/data/DowloadableDataFull_2011.01.12_reformatted.csv")
 
+# What type of cancer treatment is the most costly? What year was this?
 
+# What type of cancer treatment is the least costly? What year was this?
+
+# What is the average total cost of cancer treatment?
+
+# What was the difference in costs between the initial year after diagnosis and
+# last year of life?
+
+# Which type of cancer treatment had the highest average total cost?
+
+# What is the average annual cost increase in cancer treatment?
+
+# Reflection ------------------------------------------------------------
 # Write a paragraph of reflection on the summary information calculated by your
 # summary information function.
