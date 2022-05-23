53d26e509ec76a59ade64044f9b35a7919c0b8c0
bnguy208 <bnguy208@uw.edu>
2022-05-23 15:30:02 -0700

    table completed

--- table_summary.R
+++ table_summary.R
@@ -1,7 +1,18 @@
 library(dplyr)
 
-diabetes_df <- read.csv("/Users/daniella/desktop/INFO 201/final-projects-daniellatsing/data/diabetes_012_health_indicators_BRFSS2015.csv")
+setwd("C:/Users/brand/INFO 201/final-projects-daniellatsing/data")
+diabetes_df <- read.csv("diabetes_012_health_indicators_BRFSS2015.csv")
 
 summary_table <- group_by(diabetes_df, Diabetes_012) %>%
-  summarise(meanAge = mean(Age), meanBMI = mean(BMI), meanIncome = mean(Income)) %>% 
+  summarise(meanAge = mean(Age), meanBMI = mean(BMI), meanIncome = mean(Income), 
+            highCholesterol = sum(HighChol), highBP = sum(HighBP), 
+            Avg.Days.Poor.Phys.Health = mean(PhysHlth), 
+            Avg.Days.Poor.Mental.Health = mean(MentHlth)) %>% 
   round(digits = 0)
+
+summary_table$Diabetes_012[1] <- "No Diabetes" 
+summary_table$Diabetes_012[2] <- "Pre-Diabetes" 
+summary_table$Diabetes_012[3] <- "Diabetes" 
+
+colnames(summary_table)[1] <- "Diabetes Classification"
+  
