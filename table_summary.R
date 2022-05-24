4ddfa51842a45da6324ac7b124d16fb628bff8c2
bnguy208 <bnguy208@uw.edu>
2022-05-23 17:16:00 -0700

    table completed

--- table_summary.R
+++ table_summary.R
@@ -4,7 +4,9 @@ setwd("C:/Users/brand/INFO 201/final-projects-daniellatsing/data")
 diabetes_df <- read.csv("diabetes_012_health_indicators_BRFSS2015.csv")
 
 summary_table <- group_by(diabetes_df, Diabetes_012) %>%
-  summarise(meanAge = mean(Age), meanBMI = mean(BMI), meanIncome = mean(Income), 
+  summarise(numRespondants = length(Diabetes_012), 
+            meanAge = mean(Age), 
+            meanBMI = mean(BMI), meanIncome = mean(Income), 
             highCholesterol = sum(HighChol), highBP = sum(HighBP), 
             Avg.Days.Poor.Phys.Health = mean(PhysHlth), 
             Avg.Days.Poor.Mental.Health = mean(MentHlth)) %>% 
