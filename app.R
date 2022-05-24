62a659812043023503bd05f54d831f07035b1bb1
daniellatsing <102693164+daniellatsing@users.noreply.github.com>
2022-05-23 19:42:28 -0700

    shiny file added

---
+++ app.R
@@ -0,0 +1,17 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+# Define UI
+ui <- fluidPage(
+  
+)
+
+# Define server logic
+server <- function(input, output){
+  
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
