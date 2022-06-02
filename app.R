ba231e6e345b9dc3381ac98419431b58cd0f590b
bnguy208 <bnguy208@uw.edu>
2022-06-02 14:19:56 -0700

    bar charts created

--- app.R
+++ app.R
@@ -54,45 +54,141 @@ healthIndicators_df <- read.csv("diabetes_012_health_indicators_BRFSS2015.csv")
 # cleaned dataframes
 diabetes_df <- na.omit(healthIndicators_df) %>%
   filter(Diabetes_012 == 2) %>%
-  select(Income, Age, Sex, BMI, Smoker, HighBP, HighChol, HeartDiseaseorAttack, PhysHlth, MentHlth)
+  select(Income, Age, Sex, BMI, Smoker, HighBP, HighChol, HeartDiseaseorAttack)
 
 no_diabetes_df <- na.omit(healthIndicators_df) %>%
   filter(Diabetes_012 == 0) %>%
-  select(Income, Age, Sex, BMI, Smoker, HighBP, HighChol, HeartDiseaseorAttack, PhysHlth, MentHlth)
+  select(Income, Age, Sex, BMI, Smoker, HighBP, HighChol, HeartDiseaseorAttack)
 
 pre_diabetes_df <- na.omit(healthIndicators_df) %>%
   filter(Diabetes_012 == 1) %>%
-  select(Income, Age, Sex, BMI, Smoker, HighBP, HighChol, HeartDiseaseorAttack, PhysHlth, MentHlth)
+  select(Income, Age, Sex, BMI, Smoker, HighBP, HighChol, HeartDiseaseorAttack)
 
 # income data
 diabetes_income_df <- count(diabetes_df, 'Income')  
-diabetes_income_df$classification <- c("Diabetes", "Diabetes", "Diabetes", "Diabetes", "Diabetes", "Diabetes", "Diabetes", "Diabetes")
+diabetes_income_df$classification <- rep("Diabetes", 8) 
 
 no_diabetes_income_df <- count(no_diabetes_df, 'Income')
-no_diabetes_income_df$classification <- c("No Diabetes", "No Diabetes", "No Diabetes", "No Diabetes", "No Diabetes", "No Diabetes", "No Diabetes", "No Diabetes")
+no_diabetes_income_df$classification <- rep("No Diabetes", 8) 
 
 pre_diabetes_income_df <- count(pre_diabetes_df, 'Income')
-pre_diabetes_income_df$classification <- c("Pre-Diabetes", "Pre-Diabetes", "Pre-Diabetes", "Pre-Diabetes", "Pre-Diabetes", "Pre-Diabetes", "Pre-Diabetes", "Pre-Diabetes")
+pre_diabetes_income_df$classification <- rep("Pre-Diabetes", 8) 
 
 income_df <- bind_rows(no_diabetes_income_df, pre_diabetes_income_df, diabetes_income_df)
 
 # age data
+diabetes_age_df <- count(diabetes_df, 'Age')
+diabetes_age_df$classification <- rep("Diabetes", 13) 
+
+no_diabetes_age_df <- count(no_diabetes_df, 'Age')
+no_diabetes_age_df$classification <- rep("No Diabetes", 13) 
+
+pre_diabetes_age_df <- count(pre_diabetes_df, 'Age')
+pre_diabetes_age_df$classification <- rep("Pre-Diabetes", 13) 
+
+age_df <- bind_rows(no_diabetes_age_df, pre_diabetes_age_df, diabetes_age_df)
 
 # sex data
+diabetes_sex_df <- count(diabetes_df, 'Sex')
+diabetes_sex_df$Sex[1] <- "Female"
+diabetes_sex_df$Sex[2] <- "Male"
+diabetes_sex_df$classification <- rep("Diabetes", 2) 
+
+no_diabetes_sex_df <- count(no_diabetes_df, 'Sex')
+no_diabetes_sex_df$Sex[1] <- "Female"
+no_diabetes_sex_df$Sex[2] <- "Male"
+no_diabetes_sex_df$classification <- rep("No Diabetes", 2) 
+
+pre_diabetes_sex_df <- count(pre_diabetes_df, 'Sex')
+pre_diabetes_sex_df$Sex[1] <- "Female"
+pre_diabetes_sex_df$Sex[2] <- "Male"
+pre_diabetes_sex_df$classification <- rep("Pre-Diabetes", 2) 
+
+sex_df <- bind_rows(no_diabetes_sex_df, pre_diabetes_sex_df, diabetes_sex_df)
 
 # bmi data
+diabetes_bmi_df <- count(diabetes_df, 'BMI')
+diabetes_bmi_df$classification <- rep("Diabetes", 77) 
+
+no_diabetes_bmi_df <- count(no_diabetes_df, 'BMI')
+no_diabetes_bmi_df$classification <- rep("No Diabetes", 81) 
+
+pre_diabetes_bmi_df <- count(pre_diabetes_df, 'BMI')
+pre_diabetes_bmi_df$classification <- rep("Pre-Diabetes", 59) 
+
+bmi_df <- bind_rows(no_diabetes_bmi_df, pre_diabetes_bmi_df, diabetes_bmi_df)
 
 # smoking habit data
+diabetes_smoker_df <- count(diabetes_df, 'Smoker')
+diabetes_smoker_df$Smoker[1] <- "No"
+diabetes_smoker_df$Smoker[2] <- "Yes"
+diabetes_smoker_df$classification <- rep("Diabetes", 2) 
+
+no_diabetes_smoker_df <- count(no_diabetes_df, 'Smoker')
+no_diabetes_smoker_df$Smoker[1] <- "No"
+no_diabetes_smoker_df$Smoker[2] <- "Yes"
+no_diabetes_smoker_df$classification <- rep("No Diabetes", 2) 
+
+pre_diabetes_smoker_df <- count(pre_diabetes_df, 'Smoker')
+pre_diabetes_smoker_df$Smoker[1] <- "No"
+pre_diabetes_smoker_df$Smoker[2] <- "Yes"
+pre_diabetes_smoker_df$classification <- rep("Pre-Diabetes", 2) 
+
+smoker_df <- bind_rows(no_diabetes_smoker_df, pre_diabetes_smoker_df, diabetes_smoker_df)
 
 # bp data
+diabetes_bp_df <- count(diabetes_df, 'HighBP')
+diabetes_bp_df$HighBP[1] <- "No"
+diabetes_bp_df$HighBP[2] <- "Yes"
+diabetes_bp_df$classification <- rep("Diabetes", 2) 
+
+no_diabetes_bp_df <- count(no_diabetes_df, 'HighBP')
+no_diabetes_bp_df$HighBP[1] <- "No"
+no_diabetes_bp_df$HighBP[2] <- "Yes"
+no_diabetes_bp_df$classification <- rep("No Diabetes", 2) 
+
+pre_diabetes_bp_df <- count(pre_diabetes_df, 'HighBP')
+pre_diabetes_bp_df$HighBP[1] <- "No"
+pre_diabetes_bp_df$HighBP[2] <- "Yes"
+pre_diabetes_bp_df$classification <- rep("Pre-Diabetes", 2) 
+
+bp_df <- bind_rows(no_diabetes_bp_df, pre_diabetes_bp_df, diabetes_bp_df)
 
 # cholesterol data
+diabetes_chol_df <- count(diabetes_df, 'HighChol')
+diabetes_chol_df$HighChol[1] <- "No"
+diabetes_chol_df$HighChol[2] <- "Yes"
+diabetes_chol_df$classification <- rep("Diabetes", 2) 
+
+no_diabetes_chol_df <- count(no_diabetes_df, 'HighChol')
+no_diabetes_chol_df$HighChol[1] <- "No"
+no_diabetes_chol_df$HighChol[2] <- "Yes"
+no_diabetes_chol_df$classification <- rep("No Diabetes", 2) 
+
+pre_diabetes_chol_df <- count(pre_diabetes_df, 'HighChol')
+pre_diabetes_chol_df$HighChol[1] <- "No"
+pre_diabetes_chol_df$HighChol[2] <- "Yes"
+pre_diabetes_chol_df$classification <- rep("Pre-Diabetes", 2) 
+
+chol_df <- bind_rows(no_diabetes_chol_df, pre_diabetes_chol_df, diabetes_chol_df)
 
 # heart problems data
+diabetes_heart_df <- count(diabetes_df, 'HeartDiseaseorAttack')
+diabetes_heart_df$HeartDiseaseorAttack[1] <- "No"
+diabetes_heart_df$HeartDiseaseorAttack[2] <- "Yes"
+diabetes_heart_df$classification <- rep("Diabetes", 2) 
 
-# physical health data
+no_diabetes_heart_df <- count(no_diabetes_df, 'HeartDiseaseorAttack')
+no_diabetes_heart_df$HeartDiseaseorAttack[1] <- "No"
+no_diabetes_heart_df$HeartDiseaseorAttack[2] <- "Yes"
+no_diabetes_heart_df$classification <- rep("No Diabetes", 2)
 
-# mental health data
+pre_diabetes_heart_df <- count(pre_diabetes_df, 'HeartDiseaseorAttack')
+pre_diabetes_heart_df$HeartDiseaseorAttack[1] <- "No"
+pre_diabetes_heart_df$HeartDiseaseorAttack[2] <- "Yes"
+pre_diabetes_heart_df$classification <- rep("Pre-Diabetes", 2)
+
+heart_df <- bind_rows(no_diabetes_heart_df, pre_diabetes_heart_df, diabetes_heart_df)
 
 # Page 1---------------------------------------------------------------
 
@@ -102,6 +198,41 @@ plot1 <- ggplot(data = income_df, aes(x = Income, y = freq, fill = classificatio
   labs(title = "Median Household Income on Scale of 1-8", x = "Scale", y = "Frequency")
 plot(plot1)
 
+plot1b <- ggplot(data = age_df, aes(x = Age, y = freq, fill = classification)) +
+  geom_col(position = position_dodge())  +
+  labs(title = "Age & Diabetes Classification", x = "Age (years)", y = "Frequency")
+plot(plot1b)
+
+plot1c <- ggplot(data = sex_df, aes(x = Sex, y = freq, fill = classification)) +
+  geom_col(position = position_dodge())  +
+  labs(title = "Sex & Diabetes Classification", x = "Sex", y = "Frequency")
+plot(plot1c)
+
+plot1d <- ggplot(data = bmi_df, aes(x = BMI, y = freq, fill = classification)) +
+  geom_col(position = position_dodge())  +
+  labs(title = "BMI & Diabetes Classification", x = "BMI", y = "Frequency")
+plot(plot1d)
+
+plot1e <- ggplot(data = smoker_df, aes(x = Smoker, y = freq, fill = classification)) +
+  geom_col(position = position_dodge())  +
+  labs(title = "Smoking Habits", x = "Has Smoked At Least 100 Cigarettes in Lifetime", y = "Frequency")
+plot(plot1e)
+
+plot1f <- ggplot(data = bp_df, aes(x = HighBP, y = freq, fill = classification)) +
+  geom_col(position = position_dodge())  +
+  labs(title = "High Blood Pressure & Diabetes Classification", x = "Has High Blood Pressure", y = "Frequency")
+plot(plot1f)
+
+plot1g <- ggplot(data = chol_df, aes(x = HighChol, y = freq, fill = classification)) +
+  geom_col(position = position_dodge())  +
+  labs(title = "High Cholesterol & Diabetes Classification", x = "Has High Cholesterol", y = "Frequency")
+plot(plot1g)
+
+plot1h <- ggplot(data = heart_df, aes(x = HeartDiseaseorAttack, y = freq, fill = classification)) +
+  geom_col(position = position_dodge())  +
+  labs(title = "Heart Complications", x = "Had Coronary Heart Disease or Myocardial Infarction", y = "Frequency")
+plot(plot1h)
+
 # create the tab
 bar_chart_tab <- tabPanel(
   "Bar Chart Comparison",
@@ -110,18 +241,23 @@ bar_chart_tab <- tabPanel(
   sidebarLayout(
     sidebarPanel(
       # Select factor to analyze on bar graph
-      selectInput(inputId = "select", label = strong("Select Health Indicator"),
-                  choices = colnames(diabetes_df))
+      selectInput(inputId = "select", label = strong("Select health indicator"),
+                  choices = colnames(diabetes_df), selected = "Income")
     ),
     mainPanel(
-      h3("Chart"),
-      tableOutput(outputId = "bar_chart")
+      plotOutput(outputId = "income_bar_chart")
     )
   )
 )
 
 # Page 2---------------------------------------------------------------
-# plot2 <-
+bmi_vs_income_df <- na.omit(healthIndicators_df) %>% 
+  select(Income, BMI)
+
+plot2 <- ggplot(data = bmi_vs_income_df, aes(x = Income, y = BMI)) +
+  geom_point() +
+  labs(title = "BMI vs. Income", x = "Income", y = "BMI")
+plot(plot2)
 
 scatter_plot_tab <- tabPanel(
   "Scatter Plots",
@@ -175,8 +311,8 @@ ui <- navbarPage(
 # Define server logic
 server <- function(input, output){
   # Define bar chart to render in the UI
-  output$bar_chart <- renderTable({
-    
+  output$income_bar_chart <- renderPlot({
+    plot1
   })
   
 }
