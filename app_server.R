29bf1a704f5d1fecb0f94892a31a09f6e3a9fa4a
daniellatsing <102693164+daniellatsing@users.noreply.github.com>
2022-06-02 16:44:06 -0700

    separate files

---
+++ app_server.R
@@ -0,0 +1,23 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(bslib)
+library(stringr)
+library(plyr)
+
+# Define server logic
+server <- function(input, output){
+  # Define bar chart to render in the UI
+  output$income_bar_chart <- renderPlot({
+    filtered_df <- select(merged_df, unique(input$bar_chart), freq, classification)
+    plot1 <- ggplot(data = filtered_df, aes(x = filtered_df[1], y = freq, fill = classification)) +
+      geom_col(position = position_dodge())  +
+      labs(title = "Median Household Income on Scale of 1-8", x = input$bar_chart, y = "Frequency")
+    plot1
+  })
+  output$income_table <- renderTable({
+    nearPoints(income_df, input$income_bar_click, xvar = "Income", yvar = "freq")
+  })
+  
+}
